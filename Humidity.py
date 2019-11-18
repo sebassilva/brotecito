a936dd552fca48c40c1d15a2e67a2dad3ed20b8c
Sebastián Silva <sebastian@booxlab.com>
2019-11-18 16:29:06 -0600

    Added light

--- Humidity.py
+++ Humidity.py
@@ -9,16 +9,14 @@ class Humidity():
         GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
         GPIO.add_event_callback(channel, self.callback)  # assign function to GPIO PIN, Run function on change
  
- 
     def callback(self, channel):
         self.is_humid = not self.is_humid
 
     def getValue(self):
         return self.is_humid
  
-
-humidity = Humidity()
-
-while True:
-    print("humedad actual: " + str(humidity.getValue()))
-    #time.sleep(1)
+def test():
+    humidity = Humidity()
+    while True:
+        print("humedad actual: " + str(humidity.getValue()))
+        #time.sleep(1)
---
+++ Light.py
@@ -0,0 +1,20 @@
+from gpiozero import LightSensor, PWMLED
+from signal import pause
+from time import sleep
+
+class Light():
+    def __init__(self, sensor_pin=18):
+        sensor = LightSensor(sensor_pin)
+
+    def getValue(self):
+        return self.sensor.value()
+
+
+def test():
+    light = Light()
+    while True:
+        print("Luz actual: " + str(light.getValue()))
+        sleep(0.5)
+
+
+test()
