cefa1dfca95712930fe5bfaf84887394579f21ae
Sebastián Silva <sebastian@booxlab.com>
2019-11-18 19:17:44 -0600

    testing

--- Control.py
+++ Control.py
@@ -13,8 +13,6 @@ class Control():
         self.motor = actuators.get('motor')  # instance of motor
         self.water = actuators.get('water')  # instance of water
 
-    def printInfo(self):
-        return ", ".join("%s: %s" % item for item in self.items())
 
 
     def update(self, data): 
@@ -24,7 +22,6 @@ class Control():
         # Si esta cubierta y recibe luz, es de dia. Incrementamos time_of_light
         if not self.is_covered and self.light >= self.MIN_LIGHT:
             self.time_of_light += 1
-        self.printInfo()
 
 
     # Regresa True si esta seca la planta
@@ -32,7 +29,6 @@ class Control():
     def should_water_plant(self):
         if self.humidity <= MIN_HUMIDITY:
             return True
-        self.printInfo()
         return False
     
     # Controla el motor si ya recibio suficiente luz
@@ -40,7 +36,6 @@ class Control():
     def should_cover_plant(self):
         if self.light >= MAX_LIGHT:
             return True
-        self.printInfo()
         return False
 
     def activate(self):
