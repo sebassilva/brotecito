3c41eb5393323af4c95f38e4fabf846d75811d7a
Sebastián Silva <sebastian@booxlab.com>
2019-11-18 18:09:03 -0600

    Fixed threading

--- Valve.py
+++ Valve.py
@@ -4,24 +4,21 @@ import threading
 
 
 def openValve(self, motor):
+        print("Abriendo Valvula")
         motor.forward()
         sleep(5)
+        print("Cerrando Valvula")
+
         
 
 class Valve():
     def __init__(self):
         self.motor = Motor(forward=4, backward=14)
     
-    def openValveThread(self):
+    def open(self):
         print("Abriendo Valvula")
         x = threading.Thread(target=openValve, args=(self.motor,))
         x.start()
-
-        print("Cerrando valvula")
-    
-    def open(self):
-        thread.start_new_thread(self.openValveThread, ())
-
 
 
 def test():
